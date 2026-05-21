55293c01cceb113e6ed06f56100e9e246615b2a5
Radoslav Tomas <radoslav.tomas@gmail.com>
2026-05-21 13:42:37 +0100

    Initial commit

---
+++ database/migrations/2026_11_17_200801_create_settings_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('page_title');
+            $table->string('page_subtitle');
+            $table->text('page_description');
+            $table->text('page_keywords');
+            $table->string('address_line1');
+            $table->string('address_line2');
+            $table->string('address_line3');
+            $table->string('postcode');
+            $table->string('phone');
+            $table->string('email');
+            $table->text('map');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void {
+        Schema::dropIfExists('settings');
+    }
+};
---
+++ database/migrations/2026_11_26_131538_create_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('page_id')->nullable();
+            $table->foreignId('navigation_id');
+            $table->boolean('is_dynamic');
+            $table->string('name');
+            $table->string('url');
+            $table->integer('position');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void {
+        Schema::dropIfExists('categories');
+    }
+};
